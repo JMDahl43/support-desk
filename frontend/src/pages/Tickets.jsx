69ea91dd4fac571e5fad44b592f5fbc84551af40
JDahl <jmdahl43@gmail.com>
2022-07-03 23:32:12 -0500

    Listing tickets in UI.

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getTickets, reset } from '../features/tickets/ticketSlice'
 import Spinner from '../components/Spinner'
+import TicketItem from '../components/TicketItem'
 import BackButton from '../components/BackButton'
 
 function Tickets() {
@@ -27,6 +28,22 @@ function Tickets() {
     return <Spinner />
   }
 
-  return <h1>Tickets</h1>
+  return (
+    <>
+      <BackButton url='/' />
+      <h1>Tickets</h1>
+      <div className='tickets'>
+        <div className='ticket-headings'>
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+        {tickets.map((ticket) => (
+          <TicketItem key={ticket._id} ticket={ticket} />
+        ))}
+      </div>
+    </>
+  )
 }
 export default Tickets
